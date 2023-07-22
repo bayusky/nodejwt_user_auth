ae1740c372e38892fc0ce919d93ed104a4b4769e
bayusky <bayusangkaya@gmail.com>
2023-07-22 23:07:12 +0700

    changing API url

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,7 @@ exports.forgotPassword = async (req, res, next) => {
 
         await user.save()
 
-        const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`
+        const resetUrl = `http://localhost:3000/api/auth/passwordreset/${resetToken}`
 
         const message = `
             <h1>You have requested a password reset</h1>
